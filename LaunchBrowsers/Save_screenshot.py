bd0a07076b74d14e88700c68f172ca7c7bda77f9
Sachin <you@example.com>
2020-05-15 18:43:12 +0530

    first commit ^W

---
+++ LaunchBrowsers/Save_screenshot.py
@@ -0,0 +1,11 @@
+
+import os
+from selenium import webdriver
+
+chrome_driver_path = os.path.dirname(__file__)  + "\chromedriver.exe"
+ 
+driver=webdriver.Chrome(chrome_driver_path)
+driver.get('https://python.org')
+driver.save_screenshot("capture.png")
+ 
+driver.close()
